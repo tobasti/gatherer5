90c4855af5341e310878b5b043540c4c5b51fab3
Tobasti <tobasti@riseup.net>
2018-12-30 22:36:26 +0100

    Introduce completion times

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -12,3 +12,24 @@ RSpec.describe Task do
     expect(task).to be_complete
   end
 end
+
+describe "velocity" do
+  let (:task) { Task.new(size: 3) }
+
+  it "does not count an incomplete task towards velocity" do
+    expect(task).not_to be_a_part_of_velocity
+    expect(task.points_towards_velocity).to eq(0)
+  end
+
+  it "counts a recently completed task towards velocity" do
+    task.mark_completed(1.day.ago)
+    expect(task).to be_a_part_of_velocity
+    expect(task.points_towards_velocity).to eq(3)
+  end
+
+  it "does not count a long-ago completed task towards velocity" do
+    task.mark_completed(6.month.ago)
+    expect(task).not_to be_a_part_of_velocity
+    expect(task.points_towards_velocity).to eq(0)
+  end
+end
